9c0916727400b11c33e9a288f0a18ff45cdb63b1
hanna3-14 <114485436+hanna3-14@users.noreply.github.com>
2022-12-03 06:51:23 +0100

    day 3 task 2

--- day3/rucksack.py
+++ day3/rucksack.py
@@ -1,3 +1,5 @@
+import re
+
 f = open("rucksack.txt", "r")
 lines = f.readlines()
 
@@ -15,3 +17,22 @@ for line in lines:
 		sum += ord(doubleLetter) - ord('a') + 1
 
 print(sum)
+
+sum2 = 0
+
+groups = [lines[i:i+3] for i in range(0, len(lines), 3)]
+
+for group in groups:
+	rucksack1 = re.sub("\n", "", group[0])
+	rucksack2 = re.sub("\n", "", group[1])
+	rucksack3 = re.sub("\n", "", group[2])
+
+	commonItem = set(rucksack1).intersection(rucksack2).intersection(rucksack3)
+	commonLetter = commonItem.pop()
+
+	if commonLetter.isupper():
+		sum2 += ord(commonLetter) - ord('A') + 27
+	else:
+		sum2 += ord(commonLetter) - ord('a') + 1	
+
+print(sum2)
