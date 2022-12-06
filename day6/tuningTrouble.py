e1aa3858b88221fec869b1c1c58196ceec9f1d7f
hanna3-14 <114485436+hanna3-14@users.noreply.github.com>
2022-12-06 07:03:17 +0100

    day 6 task 2

--- day6/tuningTrouble.py
+++ day6/tuningTrouble.py
@@ -5,7 +5,20 @@ inputString = f.read()
 
 letters = list(inputString)
 
+# task 1
 for i in range(len(letters) - 4):
 	if letters[i] != letters[i + 1] and letters[i] != letters[i + 2] and letters[i] != letters[i + 3] and letters[i + 1] != letters[i + 2] and letters[i + 1] != letters[i + 3] and letters[i + 2] != letters[i + 3]:
 		print(i + 4)
+		break
+
+# task 2
+for i in range(len(letters) - 14):
+	sublist = letters[i:i + 14]
+	seenElements = []
+	for j in range(len(sublist)):
+		if not sublist[j] in seenElements:
+			seenElements.append(sublist[j])
+
+	if len(seenElements) == len(sublist):
+		print(i + 14)
 		break
