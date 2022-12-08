f21c123d9d2bfc22ad05ed746b36b1058e5b64ef
hanna3-14 <114485436+hanna3-14@users.noreply.github.com>
2022-12-08 14:30:21 +0100

    day 8 task 2

--- day8/treehouse.py
+++ day8/treehouse.py
@@ -12,6 +12,7 @@ for line in lines:
 
 inverted = list(map(list, zip(*listOfLists)))
 
+# task 1
 for i, row in enumerate(listOfLists):
 	row.pop()
 	for j, tree in enumerate(row):
@@ -32,3 +33,44 @@ for i, row in enumerate(listOfLists):
 				totalcount += 1
 
 print(totalcount)
+
+# task 2
+scenicscores = []
+
+for i, row in enumerate(listOfLists):
+	for j, tree in enumerate(row):
+		if (not i == 0) and (not j == 0) and (not j == len(row) - 1) and (not i == len(listOfLists) - 1):
+			rowpoints = []
+			sublist1 = row[0:j]
+			sublist1.reverse()
+			sublist2 = row[j + 1:]
+			sublist3 = inverted[j][0:i]
+			sublist3.reverse()
+			sublist4 = inverted[j][i + 1:]
+			for k, element in enumerate(sublist1):
+				if sublist1[k] >= listOfLists[i][j]:
+					rowpoints.append(k + 1)
+					break
+				elif k == len(sublist1) - 1:
+					rowpoints.append(k + 1)
+			for k, element in enumerate(sublist2):
+				if sublist2[k] >= listOfLists[i][j]:
+					rowpoints.append(k + 1)
+					break
+				elif k == len(sublist2) - 1:
+					rowpoints.append(k + 1)
+			for k, element in enumerate(sublist3):
+				if sublist3[k] >= listOfLists[i][j]:
+					rowpoints.append(k + 1)
+					break
+				elif k == len(sublist3) - 1:
+					rowpoints.append(k + 1)
+			for k, element in enumerate(sublist4):
+				if sublist4[k] >= listOfLists[i][j]:
+					rowpoints.append(k + 1)
+					break
+				elif k == len(sublist4) - 1:
+					rowpoints.append(k + 1)
+			scenicscores.append(rowpoints[0] * rowpoints[1] * rowpoints[2] * rowpoints[3])
+
+print(max(scenicscores))
