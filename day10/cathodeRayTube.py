3e9a89a7becdd1f1f4b818c700967d75ed710f75
hanna3-14 <114485436+hanna3-14@users.noreply.github.com>
2022-12-10 10:14:53 +0100

    day 10 task 2

--- day10/cathodeRayTube.py
+++ day10/cathodeRayTube.py
@@ -27,3 +27,53 @@ for line in lines:
 		x += int(chunks[1])
 
 print(totalsum)
+
+# task 2
+x = 1
+sprite = []
+grid = [[None] * 40] * 6
+i = 0
+cycle = 0
+
+for line in lines:
+	re.sub("\n", "", line)
+	chunks = line.split(' ')
+
+	sprite = [x - 1, x, x + 1]
+
+	if re.match('noop', line):
+		if sprite[0] == cycle or sprite[1] == cycle or sprite[2] == cycle:
+			grid[i][cycle] = "#"
+		else:
+			grid[i][cycle] = "."
+		cycle += 1
+		if cycle == 40:
+			for j in range(len(grid[i])):
+				print(grid[i][j], end="")
+			print("")
+			i += 1
+			cycle = 0
+	elif re.match('addx', line):
+		if sprite[0] == cycle or sprite[1] == cycle or sprite[2] == cycle:
+			grid[i][cycle] = "#"
+		else:
+			grid[i][cycle] = "."
+		cycle += 1
+		if cycle == 40:
+			for j in range(len(grid[i])):
+				print(grid[i][j], end="")
+			print("")
+			i += 1
+			cycle = 0
+		if sprite[0] == cycle or sprite[1] == cycle or sprite[2] == cycle:
+			grid[i][cycle] = "#"
+		else:
+			grid[i][cycle] = "."
+		cycle += 1
+		if cycle == 40:
+			for j in range(len(grid[i])):
+				print(grid[i][j], end="")
+			print("")
+			i += 1
+			cycle = 0
+		x += int(chunks[1])
